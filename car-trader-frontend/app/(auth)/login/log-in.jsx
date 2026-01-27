e4b274dfb68051ffac3afce414ff0ae3332d739a
mkkarekezi <m.kkarekezi@gmail.com>
2026-01-28 01:37:45 +0200

    commit

--- car-trader-frontend/app/(auth)/login/log-in.jsx
+++ car-trader-frontend/app/(auth)/login/log-in.jsx
@@ -33,23 +33,12 @@ export function LoginAccount(router) {
       const data = await response.json();
       console.log("Login response:", data);
 
-      // if (data.success) {
-      //   localStorage.setItem("token", data.token);
-      //   router.push("/dashboard/upload");
-      // } else {
-      //   setError(data.message || "Login failed");
-      // }
-
       if (data.success) {
-        console.log("Login data:", data); // Debug - see what backend sends
-
         if (data.token) {
           localStorage.setItem("token", data.token);
-          console.log("Token stored:", data.token); // Debug
-          // router.push("/dashboard/upload");
+          router.push("/dashboard/upload");
         } else {
           setError("No token received from server");
-          console.error("No token in response:", data); // Debug
         }
       }
     } catch (err) {
